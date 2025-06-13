dcb1a73b3a54c37738f963389374ea5943879bb3
bima672003 <bimagalihsaputa03@gmail.com>
2025-06-13 16:03:28 +0700

    membuat ImageController dan View updoad

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\KendaraanController;
 use App\Http\Controllers\TransaksiServisController;
 use App\Http\Controllers\PekerjaanServisController;
-
+use App\Http\Controllers\ImageController;  
 
 Route::resource('antrian', AntrianController::class);
 Route::resource('pelanggan', PelangganController::class);
@@ -22,3 +22,6 @@ Route::get('/', function () {
 Route::get('/pendaftaran-ktp', function () {
     return 'Selamat datang di halaman Pendaftaran KTP Online';
 })->middleware('check.age');
+
+ Route::get('/upload', [ImageController::class, 'create']);
+ Route::post('/upload', [ImageController::class, 'store'])->name('image.upload');
